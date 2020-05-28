fba9a0897e91bc7a17c3c18d906cdad95894999f
yulia9112 <egorova_yulka@mail.ru>
2020-05-28 17:57:39 +0300

    deadlock1

--- lab5/src/deadlock.c
+++ lab5/src/deadlock.c
@@ -3,26 +3,44 @@
 
 pthread_mutex_t first_variable_mutex;
 pthread_mutex_t second_variable_mutex;
+pthread_mutex_t third_variable_mutex;
+ 
 static int first_variable = 1;
 static int second_variable = 1;
+static int third_variable = 1;
 
 void *deadlock_first(void *args) {
   pthread_mutex_lock(&first_variable_mutex);
   first_variable++;
   second_variable++;
+  third_variable++;
+
+  pthread_mutex_lock(&second_variable_mutex);
+  first_variable++;
+  second_variable++;
+  third_variable++;
+
+
+  pthread_mutex_lock(&second_variable_mutex);
+  first_variable++;  
+  second_variable++;
+  third_variable++;  
+ 
 }
 
 
 int main() {
 
-  pthread_t threads[2];
+  pthread_t threads[3];
   pthread_mutex_init(&first_variable_mutex, NULL);
   pthread_mutex_init(&second_variable_mutex, NULL);
+  pthread_mutex_init(&third_variable_mutex, NULL); 
   pthread_create(&threads[0], NULL, deadlock_first, NULL);
   pthread_create(&threads[1], NULL, deadlock_first, NULL);
-  for (int i = 1; i< 2; i++) {
+  pthread_create(&threads[2], NULL, deadlock_first, NULL);   
+  for (int i = 1; i< 3; i++) {
      pthread_join(threads[i], NULL);
   }
-  printf("First - %i\nSecond - %i\n", first_variable, second_variable);
+  printf("First - %i\nSecond - %i\nThird - %i\n", first_variable, second_variable, third_variable);
   return 0;
  }
